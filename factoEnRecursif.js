a017e855a101c2069ea96be1e444cc8098889a12
kardev38 <moujahedk83@gmail.com>
2021-04-03 17:00:41 +0200

    encore un peu

--- factoEnRecursif.js
+++ factoEnRecursif.js
@@ -2,32 +2,11 @@
 // Ex. d’appel du script :
 // $> ./factoriel 3
 // 6
-  
-
-var readline = require("readline-sync");
+  var readline = require("readline-sync");
 var factorielle = readline.questionInt("Quelle factorielle voulez-vous : ");
 var resultat = 1 ;
-
 for(var i = 1 ; i <= factorielle ; i++){
         resultat = resultat * i ;
-    console.log(resultat );
+      
 }
-    
-    
-    
-//         if (n<=1) {
-//         return 1
-//     } else 
-//             {return n*resultat(n-1)}
-//     }
-
-    
-//     function fact(nbr){
-//         var i, nbr, f = 1;
-//         for(i = 1; i <= nbr; i++)  
-//         {
-//           f = f * i;   // ou f *= i;
-//         }  
-//         return f;
-//       }
-//       console.log(factorielle  * fact());
+        console.log(resultat ); 
