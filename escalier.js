13408e26ab57a869c290a0724ff5360260cffef9
kardev38 <moujahedk83@gmail.com>
2021-04-04 13:44:48 +0200

    optimiser escalier

--- escalier.js
+++ escalier.js
@@ -1,10 +1,6 @@
 var readline = require("readline-sync");
-var saisir = readline.question("saisir un argument : ");
-var argument = saisir;
-var espace  = argument;
-var diese = "#";
-var espace = " ";
+var nb_etages = readline.questionInt("Saisir un argument : ");
 
-for(var i = 0 ; i < saisir ; i++){
-    console.log(espace.repeat(argument - i) + diese.repeat(i+1));
+for(var i = 0 ; i < nb_etages ; i++){
+    console.log(" ".repeat(nb_etages - i) + "#".repeat(i+1));
 }
