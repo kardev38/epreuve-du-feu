0d6335745e3c1f4da1be2ba174e05f59634bfa59
kardev38 <moujahedk83@gmail.com>
2021-04-06 17:24:33 +0200

    je recomence

--- tri.js
+++ tri.js
@@ -3,18 +3,8 @@
 // Bonus : implémenter plusieurs algorithme de tri (tri à bulle, tri sélectif, ... )
 // Ex. d’appel du script :
 // $> ./tri 4 5 1 3 2
-// 5 4 3 2 1
+// // 5 4 3 2 1
  
+const tab = process.argv.slice(2)
 
 
-// var readline = require("readline-sync");
-// var saisir = readline.question("Quel nom d'uilisateur voulez-vous : "  );
-//  saisir;
-var readline = require("readline-sync");
-var valeur = readline.questionInt("Quel nombre voulais-vous trie  : ");
-var tableau = valeur;
-tableau =function(a, b){
-    return a - b
-}
-
-console.log(tableau)
