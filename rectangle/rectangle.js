b6aa5a15e1da3da659bbdf70bddfa5d33419dd22
Moujahed <moujahedk83@gmail.com>
2021-04-19 13:53:41 +0200

    le debut

--- rectangle/rectangle.js
+++ rectangle/rectangle.js
@@ -4,3 +4,9 @@
 // $> ./rectangle c1.txt c2.txt
 // 2,1
 // $> cat c1.txt
+
+var fs = require('fs');
+var rec1 = fs.readFileSync('c1.txt').toString().split("\n");  
+var rec2 = fs.readFileSync('c2.txt').toString().split("\n");
+console.log(rec1);
+console.log(rec2);
