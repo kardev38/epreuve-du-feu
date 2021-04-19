1af1b4a7e34a5f94e45f07915bd5590f4639b2fb
Moujahed <moujahedk83@gmail.com>
2021-04-19 11:59:16 +0200

    terminer

---
+++ factorielRetour.js
@@ -0,0 +1,26 @@
+// Créer un script qui calcul la factoriel du nombre passé en argument, sans exposant.
+// Ex. d’appel du script :
+// $> ./factoriel 42
+// 1,405,006,117,752,879,898,543,142,606,244,511,569,936,384,000,000,000
+
+var readline = require("readline-sync");
+var factorielle = readline.questionInt("Quelle factorielle voulez-vous : ");
+
+ 
+ function facto(n){
+  
+     if( n == 1){
+          return 1;
+      }   else {
+            return n * facto(n - 1);;   
+        }
+  }
+          
+        
+
+        var optimoser = facto(factorielle)
+        optimoser = optimoser.toLocaleString()
+        console.log(optimoser)
+       
+     
+  
