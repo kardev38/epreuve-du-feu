6ebafc93c38f5e0a43b8430774b0f09d32f2318c
Moujahed <moujahedk83@gmail.com>
2021-04-22 10:03:43 +0200

    optimiser

--- escalier.js
+++ escalier.js
@@ -1,6 +1,6 @@
 var readline = require("readline-sync");
 var nb_etages = readline.questionInt("Saisir un argument : ");
 
-for(var i = 0 ; i < nb_etages ; i++){
-    console.log(" ".repeat(nb_etages - i) + "#".repeat(i+1));
+for (var i = 0; i < nb_etages; i++) {
+  console.log(" ".repeat(nb_etages - i) + "#".repeat(i + 1));
 }
