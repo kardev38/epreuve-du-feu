a30e060eac1f136f970ac12e9dd25815a22fcfab
Moujahed <moujahedk83@gmail.com>
2021-04-24 00:44:36 +0200

    optimiser

--- tri.js
+++ tri.js
@@ -23,7 +23,3 @@ function tri(array) {
 var tabtrie = tri(tab);
 console.log("saisie Trié : " + tabtrie);
 
-// premiere boucle => j=0 => 4 5 1 3 2
-// deuxieme boucle => j=1 => 4 1 5 3 2
-// troisieme boucle => j=2 => 4 1 3 5 2
-//  quatrieme boucle => j=3 => 4 1 3 2 5
