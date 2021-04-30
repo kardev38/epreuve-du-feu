4a7d10fd1a19778854ea4de7106f62a38f69c775
Moujahed <moujahedk83@gmail.com>
2021-04-30 16:02:42 +0200

    En cours

--- rectangle/rectangle.js
+++ rectangle/rectangle.js
@@ -11,26 +11,22 @@ const c2 = fs.readFileSync("./" + process.argv[3], "utf-8").split("\n");
 console.log(c1);
 console.log(c2);
 
-function positionc1dansc2(arr1, arr2){
-    for (i = 0; i < arr2.length; i++){
-        for (j = 0; j < arr2[i].length; j++){
-           if(arr2[i][j].length == arr1[0]){
-               console.log( true);
-               
-               } else{
-                console.log(false);
-               }
-           } 
-        }
+function positionc1dansc2(arr1, arr2) {
+  for (i = 0; i < arr2.length; i++) {
+    for (j = 0; j < arr2[i].length; j++) {
+        
+      if (arr2[i][j] == arr1[0][0]) {
+        console.log(arr2.includes(arr1));
+      } if (arr2[i+1][j+1] == arr1[0][1]){
+        console.log(arr2.includes(arr1));
+      }
     }
-    console.log( false || true)
+  }
+}
+console.log(c2.includes(c1));
 
-
-   
 //
-
 
 // parcourir le tableau c2 en cherchant le premier element du tableau c1
 // ensuite cherche le deuxieme element de c1 dans c2
-// .split("\r\n");
-// .split("\r\n");
+
