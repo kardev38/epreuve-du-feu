1cc32d7486345082b40c8c7f46f5bb8f0ac5a7ba
Moujahed <moujahedk83@gmail.com>
2021-05-01 22:27:14 +0200

    En cours

--- rectangle/rectangle.js
+++ rectangle/rectangle.js
@@ -13,20 +13,9 @@ console.log(c2);
 
 function positionc1dansc2(arr1, arr2) {
   for (i = 0; i < arr2.length; i++) {
-    for (j = 0; j < arr2[i].length; j++) {
-        
-      if (arr2[i][j] == arr1[0][0]) {
-        console.log(arr2.includes(arr1));
-      } if (arr2[i+1][j+1] == arr1[0][1]){
-        console.log(arr2.includes(arr1));
-      }
-    }
+    for (j = 0; j < arr2[i].length; j++) {}
   }
 }
-console.log(c2.includes(c1));
-
-//
 
 // parcourir le tableau c2 en cherchant le premier element du tableau c1
 // ensuite cherche le deuxieme element de c1 dans c2
-
