9c4df2489032dcb8c4e5e546e604c128b1c67084
Moujahed <moujahedk83@gmail.com>
2021-05-02 23:57:32 +0200

    En cours

--- rectangle/rectangle.js
+++ rectangle/rectangle.js
@@ -11,7 +11,13 @@ const c2 = fs.readFileSync("./" + process.argv[3], "utf-8").split('').map(Number
 console.log(c1);
 console.log(c2);
 
+function positionc1dansc2(arr1, arr2) {
+  for (i = 0; i < arr2.length; i++) {
+    for (j = 0; j < arr2[i].length; j++) {
 
+    }
+  }
+}
 
 // parcourir le tableau c2 en cherchant le premier element du tableau c1
 // ensuite cherche le deuxieme element de c1 dans c2
