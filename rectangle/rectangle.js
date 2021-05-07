aa5477dec0bcebe3cb1ad5809c3e4c53588d3bab
Moujahed <moujahedk83@gmail.com>
2021-05-07 23:20:16 +0200

    En cours

--- rectangle/rectangle.js
+++ rectangle/rectangle.js
@@ -18,7 +18,7 @@ function positionindes(tab1, tab2) {
     }
   }
 }
-// achicher les tableau en long dans le terminal
+// achicher les tableau en longeur dans le terminal
 // afficher les fichier en tableau avec chaque element separée "bien propre"
 // parcourir le tableau c2 en cherchant le premier element du tableau c1
 // ensuite cherche le deuxieme element de c1 dans c2
