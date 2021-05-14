36078df31b25ce0cc37f430ba4468b463169c799
Moujahed <moujahedk83@gmail.com>
2021-05-14 14:44:56 +0200

    optimiser avec pseudo code

--- escalier.js
+++ escalier.js
@@ -4,3 +4,10 @@ var nb_etages = readline.questionInt("Saisir un argument : ");
 for (var i = 0; i < nb_etages; i++) {
   console.log(" ".repeat(nb_etages - i) + "#".repeat(i + 1));
 }
+
+// crée une variable de l'argument saisi
+// dire que l'argument est le nombres darche a afficher 
+// l'argument saisi sera aussi le nombres d'iteration de la boucle
+// a chaque iteration on dira que espace sera -1 et diez +1
+
+
