adc2b0b6cb342e4738b7762ce71537d3f5cee367
Moujahed <moujahedk83@gmail.com>
2021-05-18 23:00:10 +0200

    optimiser avec pseudo code pas clair

--- escalier.js
+++ escalier.js
@@ -15,7 +15,6 @@ for (var i = 0; i < nb_etages; i++) {
   console.log(" ".repeat(nb_etages - i) + "#".repeat(i + 1));
 }
 
-// crée une variable de l'argument saisi 
 // dire que l'argument est le nombres de marche a afficher 
 // l'argument saisi sera aussi le nombres d'iteration de la boucle
 // a chaque iteration on dira que espace sera -1 et diez +1
