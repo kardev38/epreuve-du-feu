eeed4b1fdc8db54af0c4cc77b8f75273882255cd
Moujahed <moujahedk83@gmail.com>
2021-05-18 23:01:08 +0200

    optimiser + pseudo code de pierre urban

--- escalier.js
+++ escalier.js
@@ -15,8 +15,12 @@ for (var i = 0; i < nb_etages; i++) {
   console.log(" ".repeat(nb_etages - i) + "#".repeat(i + 1));
 }
 
-// dire que l'argument est le nombres de marche a afficher 
-// l'argument saisi sera aussi le nombres d'iteration de la boucle
-// a chaque iteration on dira que espace sera -1 et diez +1
-
-
+// -Faire une boucle A qui commence à 0 et qui va jusqu'à la valeur en entier du premier argument moins 1
+// -Pour chaque itération de cette boucle, faire:
+//  -Une boucle B qui commence à 0 et qui va jusqu'à la valeur en entier du premier argument moins 1
+//  Pour chaque itération de cette boucle, faire:
+//  -Un test pour vérifier si la valeur actuelle de la boucle B 
+//  est inférieure à la valeur en entier du premier argument moins la valeur actuelle de la boucle A
+//  -Si c'est vrais, alors il faut afficher un espace ( )
+//  -Sinon, il faut afficher une dièse (#)
+// -Afficher un retour à la ligne (\n)
