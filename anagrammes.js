34c79d607ecf6725a9e1af08c2f4ef71f74498b5
Moujahed <moujahedk83@gmail.com>
2021-05-23 21:46:05 +0200

    bientot le pseudo code

---
+++ anagrammes.js
@@ -0,0 +1,6 @@
+// Créer un script qui affiche tous les anagrammes du premier argument contenu dans le 
+// fichier du second arguments.
+// Ex. d’appel du script :
+
+// $> ./anagramme arbre fr.txt
+// [“arbre”, “barre”]
