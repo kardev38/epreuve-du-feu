c9336f256dd92dc1b94084bd55df444acb6eb997
Moujahed <moujahedk83@gmail.com>
2021-06-06 06:30:21 +0200

    Bientot le pseudo code

--- factoEnRecursif.js
+++ factoEnRecursif.js
@@ -18,5 +18,5 @@
         console.log(facto(factorielle));
 
         // bientot le pseudo code sur cette exercice aussi
-        // c'est pas facile d'explique de maniere explicite la recursivité
+        // c'est pas facile d'explique de maniere explicite la recursivité,
         // et de ce faire comprendre.
